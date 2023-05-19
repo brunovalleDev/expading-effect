7ad39e116460ac02239423db6672a76c8f95bb3c
Bruno Valle Silva <brunovallesilva93@gmail.com>
2023-05-19 15:07:37 -0300

    Melhoria no código JS. Agora, ao clicar em um card, a classe active é removida fazendo o elemento voltar a sua forma original

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const cards = document.querySelectorAll('.card');
 
-cards.forEach(card => {
+
+cards.forEach((card) => {
   card.addEventListener('click', () => {
+    if(card.classList.contains('active')) {
+      card.classList.remove('active');
+    } else {
     removerClasseActive();
     card.classList.add('active');
+    }
+  
   });
 });
 
 function removerClasseActive() {
-  cards.forEach(card => {
+  cards.forEach((card) => {
     card.classList.remove('active');
   });
 }
+
+
